dee9e6d0b2a0a08615f0a97c0f540b52b2469ecc
Azeez Samad <seshp@ymail.com>
2022-08-24 12:34:16 +0100

    feat(*): create app theme and styles

---
+++ src/theme/index.ts
@@ -0,0 +1,26 @@
+import { ITheme } from "./style";
+
+export const theme: ITheme = {
+  palette: {
+    background: "#FEFEFE",
+    black: "#333333",
+    black_2: "rgba(51,51,51,0.3)",
+    white: "#fff",
+    transparent: "transparent",
+    green: "#37BD3C",
+    blue_6: "#1C243A",
+    red: "#FF0000",
+    
+  },
+  breakpoints: {
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200,
+    xxl: 1400,
+  },
+};
+
+export const mq = Object.values(theme.breakpoints).map(
+  (bp) => `@media (min-width: ${bp}px)`
+);
---
+++ src/theme/style.d.ts
@@ -0,0 +1,37 @@
+import "@emotion/react";
+
+export type ThemeColors =
+  | "background"
+  | "black"
+  | "black_2"
+  | "white"
+  | "transparent"
+  | "red"
+  | "green"
+  | "blue_6";
+
+export interface ITheme {
+  palette: {
+    background: string;
+    black: string;
+    black_2: string;
+    white: string;
+    transparent: string;
+    red: string;
+    green: string;
+    blue_6: string;
+
+  };
+  breakpoints: {
+    sm: number;
+    md: number;
+    lg: number;
+    xl: number;
+    xxl: number;
+  };
+}
+
+declare module "@emotion/react" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface Theme extends ITheme {}
+}
