67579979301463810e69b20f7a9e7bd44e900b23
Azeez Samad <seshp@ymail.com>
2022-08-29 17:15:37 +0100

    feat(*) updated junior admin page

--- src/pages/junior_admins/index.tsx
+++ src/pages/junior_admins/index.tsx
@@ -1,13 +1,58 @@
-import AdminDashboardLayout from '../../layout/AdminDashboardLayout';
+/** @jsxImportSource @emotion/react */
+
+import { Button } from "../../lib/Button";
+import AdminDashboardLayout from "../../layout/AdminDashboardLayout";
+import Plus from "../../assets/icons/plus.svg";
+import { useAppSelector, useAppDispatch } from "../../hooks";
+import { toggleModal } from "../../redux/slices/ui";
+import { Modals } from "../../redux/slices/ui/types";
+import CreateJuniorAdminModal from "../../components/modal/CreateJuniorAdmin";
+import ProfileCard from "../../components/ProfileCard";
 
 const JuniorAdmin = () => {
+  const {
+    ui: { modals },
+  } = useAppSelector((state) => state);
+
+  const dispatch = useAppDispatch();
+
+  const showCreateJuniorAdminModal = () => {
+    dispatch(
+      toggleModal({
+        name: Modals.CREATE_JUNIOR_ADMIN,
+      })
+    );
+  };
   return (
     <div>
-        <AdminDashboardLayout pageTitle="Junior Admin" >
-      <h1>Junior admin</h1>
+      <AdminDashboardLayout pageTitle="Junior Admin">
+        {/* <h1>Junior admin</h1> */}
+        <div className="d-flex justify-content-between">
+          <div></div>
+
+          <Button
+            variant="green"
+            className="d-flex align-items-center fs-14 justify-content-center"
+            css={{ margin: "0px 22px 22px 2px" }}
+            onClick={showCreateJuniorAdminModal}
+          >
+            <img src={Plus} alt="" className="me-2" />
+            Create New
+          </Button>
+        </div>
+        <section id="juniorAdmin" >
+          <ProfileCard />
+        </section>
       </AdminDashboardLayout>
+      <CreateJuniorAdminModal
+        showModal={modals.createJuniorAdmin?.isOpen}
+        toggle={() =>
+          dispatch(toggleModal({ name: Modals.CREATE_JUNIOR_ADMIN }))
+        }
+        {...modals.createJuniorAdmin?.props}
+      />
     </div>
   );
-}
+};
 
 export default JuniorAdmin;
