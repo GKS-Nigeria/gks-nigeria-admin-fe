a25ec37a6db1590b00e21db77c9de160ccd7f44e
Azeez Samad <seshp@ymail.com>
2023-02-11 17:11:34 +0100

    chore(constant folder): change enpoint url from heroku to orender

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,3 +1,4 @@
-export const API_BASE_URL = process.env.PRIVATE_API_BASE_URL || "https://gks-nigeria-api.herokuapp.com/v1"
+export const API_BASE_URL = process.env.PRIVATE_API_BASE_URL || "https://gks-nigeria-api.onrender.com/v1"
+// export const API_BASE_URL = process.env.PRIVATE_API_BASE_URL || "https://gks-nigeria-api.herokuapp.com/v1"
 export const USER_CODE_KEY = "gks__code";
 export const PROTECTED_ROUTES = "/junior_admin /members /content /branch";
